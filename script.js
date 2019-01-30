bb735a3db74c79fae6ba054d6c52fdec609c8f96
Luca Naterop <luca.naterop@bluewin.ch>
2019-01-30 17:41:46 +0100

    slide to content

--- script.js
+++ script.js
@@ -44,8 +44,27 @@ function attachHoverEffects() {
   });
 }
 
+function goToStartPosition() {
+  $("html, body").animate({ scrollTop: 0 }, 50);
+  const windowWidth = window.innerWidth;
+  const imageWidth = 1300;
+  const x = (imageWidth - windowWidth) / 2;
+  $("html, body").animate({ scrollTop: 1000, scrollLeft: x }, 50);
+}
+
 $(document).ready(() => {
   positionLeftElements();
   positionRightElements();
   attachHoverEffects();
+  goToStartPosition();
 });
+
+
+
+
+
+
+
+
+
+
